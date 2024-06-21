795b0be1d1c9100185e59a41d6f48fae7a171a6e
unknown <rosa.angel.daniel@gmail.com>
2024-06-21 00:31:59 -0600

    added pop up

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+const gamelist = ['Rock' , 'Paper' , 'Scissors'];
+
+console.log(gamelist);
+
+function rps() {
+    alert("Let's play Rock Paper Scissors")
+    let choice = prompt("Please type an answer" , "Rock, Paper, Scissors");
+}
+
